0fca0d052dcd5a28860008c4d813b78da78b49c9
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-05-08 21:09:46 -0700

    fixed jaws database url

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 // Import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-var mysql = require("mysql2");
 
 require('dotenv').config();
 
+// Create connection to our database, pass in your MySQL information for username and password
+let sequelize;
+
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'geeks_db',
+    dialect: 'mysql',
     port: 3306
   });
 }
 
-connection.connect();
-
-module.exports = connection;
+module.exports = sequelize;
